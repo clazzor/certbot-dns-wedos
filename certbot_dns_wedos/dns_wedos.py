4c9fae1e2158ae3ab33e65fc492c0ceda8d31e30
clazzor <clazzor@slapnet.cz>
2023-03-08 23:10:11 +0100

    feat(dns_wedos.py): Making wedos client and minor fix

--- certbot_dns_wedos/dns_wedos.py
+++ certbot_dns_wedos/dns_wedos.py
@@ -1,14 +1,70 @@
 #!/usr/bin/python3
 
-import logging
-import requests
+import logging, requests, json, hashlib, time
+from datetime import datetime
 from certbot.plugins.dns_common import DNSAuthenticator
-
+from certbot import errors
 logger = logging.getLogger(__name__)
 
-class Authenticator(DNSAuthenticator):
+
+class _WedosClient():
+    def __init__(self, username: str, password: str) -> None:
+        self.url = "https://api.wedos.com/wapi/json"
+        self.username = username
+        self.password = password
+        self.ttl = 300
+        self.session = requests.Session()
+
+    def _getHour() -> str:
+        time = str(datetime.now().hour)
+        if len(time) == 1: time = "0" + time
+        return time
+
+    def _clientConnect(self, command: str, params: dict = None) -> requests.post:
+        time = str(datetime.now().hour)
+        if len(time) == 1: time = "0" + time
+
+        auth = (self.username + self.password + time ).encode("ascii")
+
+        data = {
+            "user": self.username,
+            "auth": hashlib.sha1(auth).hexdigest(),
+            "command": command
+            }
+
+        if type(params) is dict: data.update({"data": params})
+
+        data = json.dumps({"request": data})
+        return self.session.post(self.url, data={'request': data})
+
+    def _findID(self, data: dict, key: str) -> int:
+        ID = None
+        for row in data['data']['row']:
+            if row['rdata'] == key:
+                ID = row['ID']
+                break
+        return ID
+
+    def add_txt_record(self, domain: str, validation_name: str, validation: str) -> None:
+        record = {"domain": domain, 
+                  "name": validation_name.replace('.' + domain, ''),
+                  "type": "TXT",
+                  "ttl": self.ttl,
+                  "rdata": validation}
+
+        response = self._clientConnect("dns-row-add", record)
+        response = self._clientConnect("dns-domain-commit", {"name": domain})
+
+
+    def del_txt_record(self, domain: str, validation_name: str, validation: str) -> None:
+        response = self._clientConnect("dns-rows-list", {"domain": domain})
+        id = self._findID(json.loads(response.text)["response"],  validation)
+        response = self._clientConnect("dns-row-delete", {"domain": domain, "row_id": id})
+        response = self._clientConnect("dns-domain-commit", {"name": domain})
+
+#class Authenticator(DNSAuthenticator):
+class Authenticator():
     description = "Obtain certificates for Wedos dns servers."
-    ttl = 60
 
     def __init__(self, *args: any, **kwargs: any) -> None:
         super(Authenticator, self).__init__(*args, **kwargs)
@@ -33,7 +89,18 @@ class Authenticator(DNSAuthenticator):
         pass
 
     def _perform(self, domain: str, validation_name: str, validation: str) -> None:
-        print(f"data: {domain} -> {validation_name} \nKey: {validation}")
+        self._get_wedos_client().add_txt_record(domain, validation_name, validation)
+        time.sleep(300)
+
 
     def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
-        pass
+        self._get_wedos_client().del_txt_record(domain, validation_name, validation)
+
+    def _get_wedos_client(self) -> _WedosClient:
+        """return _WedosClient(
+            self.credentials.conf("username"),  
+            self.credentials.conf("password"),
+        )"""
+        return _WedosClient(
+            "", ""
+        )
---
+++ certbot_dns_wedos/test.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+
+import dns_wedos as xy
+
+xy.Authenticator()._perform("clazzor.eu", "_acme-challenge.clazzor.eu", "kn5cbGyQ_60EtiQ4B3PPfamBDIG0hTE8vTvlgJRsCtA")
+#xy.Authenticator()._cleanup("clazzor.eu", "_acme-challenge.clazzor.eu", "kn5cbGyQ_60EtiQ4B3PPfamBDIG0hTE8vTvlgJRsCtA")
