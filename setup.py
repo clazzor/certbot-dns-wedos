17665bd94166a9695061699f67f4fb89d6c60fd6
clazzor <clazzor@slapnet.cz>
2023-03-12 19:22:20 +0100

    refactor(setup.py): Updating version and classifiers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = "0.0.1"
+version = "0.8"
 
 install_requires = [
     "acme>=0.29.0",
@@ -24,19 +24,22 @@ setup(
     license="Apache License 2.0",
     python_requires=">=3.0",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Plugins",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Security",
-        "Topic :: System :: Installation/Setup",
-        "Topic :: System :: Networking",
-        "Topic :: System :: Systems Administration",
-        "Topic :: Utilities",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
     ],
     packages=find_packages(),
     include_package_data=True,
