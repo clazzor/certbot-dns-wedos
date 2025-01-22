bdc4a7404c0cc22b2ab4bfaadf0de19a22234d7c
clazzor <clazzor@slapnet.cz>
2025-01-22 19:18:59 +0100

    add(__init__.py): Basic info

---
+++ certbot_dns_wedos/__init__.py
@@ -0,0 +1,17 @@
+"""
+This plugin uses the Certbot DNS-01 challenge to create and delete
+TXT records on the Wedos domain server using the Wedos API called
+the WAPI. With this plugin, you can generate wildcard SSL certificates.
+
+Basic usage:
+ certbot certonly --authenticator dns-wedos \
+ --dns-wedos-propagation-seconds 420 \
+ --dns-wedos-credentials /path/to/the/file.ini \
+ -d example.com -d *.example.com
+
+Basic file.ini example:
+ dns_wedos_user=user@example.com
+ dns_wedos_auth=examplepassword
+
+GitHub: https://github.com/clazzor/certbot-dns-wedos/
+"""
