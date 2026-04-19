7f93efd63aca7dfd0111bca9667ef18b866f5d65
clazzor <clazzor@slapnet.cz>
2026-04-19 14:10:41 +0200

    Bump new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = "2.4"
+version = "2.5"
 
 install_requires = [
     "certbot>=3.0.0",
@@ -24,7 +24,7 @@ setup(
     url="https://github.com/clazzor/certbot-dns-wedos",
     author="Clazzor",
     license="Apache License 2.0",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Plugins",
@@ -33,11 +33,11 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Security",
         "Topic :: System :: Installation/Setup",
